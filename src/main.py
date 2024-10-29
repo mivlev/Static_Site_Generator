6fb6e4b09909357e3eea8d0d702417aa4c55b788
mivlev <markivlev@gmail.com>
2024-10-29 14:26:13 -0600

    Added textnode functionality

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, NodeType
+
+def main():
+    dummy = TextNode("dummy", NodeType.NORMAL, url="dummy_url")
+    print(dummy)
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,23 @@
 from enum import Enum
 
+from enum import Enum
+
 class NodeType(Enum):
-    pass
+    NORMAL = 0
+    BOLD = 1
+    ITALIC = 2
+    CODE = 3
+    LINKS = 4
+    IMAGES = 5
+
+class TextNode:
+    def __init__(self, text: str, text_type: NodeType, url: str = None):
+        self.text = text
+        self.text_type = text_type.value
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode(text={self.text}, text_type={self.text_type}, url={self.url})"
